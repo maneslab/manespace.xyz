78fdc6b93acddb9cddbf150cee435fe1ed093fe7
blackrabbit <rabbitblack944@gmail.com>
2022-08-10 17:04:51 +0800

    修改footer文案

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,12 +1,14 @@
+import DiscordIcon from 'public/img/icons/discord.svg'
+import TwitterIcon from 'public/img/icons/twitter.svg'
 export default function Footer(props) {
     return (
 		<div className="lg:flex lg:justify-between max-w-screen-xl mx-auto w-full py-4">
-			<div className="text-center text-sm lg:text-base lg:text-left">© 2022 ManeSPACE by WeirdoGhostGang</div>
-			<div className="gap-4 flex justify-center text-sm lg:text-base lg:justify-end">
+			<div className="text-center text-sm lg:text-left">© 2022 ManeSPACE by WeirdoGhostGang, powered by ManesLAB</div>
+			<div className="gap-4 flex justify-center text-sm items-center lg:justify-end">
 				<a href="https://www.manestudio.xyz" target="_blank" className="hover:text-gray-400">ManeStudio</a>
 				<a href="https://www.maneslab.xyz" target="_blank" 	className="hover:text-gray-400">ManesLAB</a>
-				<a href="https://twitter.com/maneslab" target="_blank" className="hover:text-gray-400">Twitter</a>
-				<a href="https://discord.gg/EnCUugtfVn" className="hover:text-gray-400">Discord</a>
+				<a href="https://twitter.com/manestudioxyz" target="_blank" className="hover:text-gray-400"><TwitterIcon className="icon-xs"/></a>
+				<a href="https://discord.gg/EnCUugtfVn" target="_blank" className="hover:text-gray-400"><DiscordIcon className="icon-xs"/></a>
 			</div>
 		</div>
     )
