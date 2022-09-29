894c5fb7b4551e2328af764328c439bd3edd3f51
blackrabbit <rabbitblack944@gmail.com>
2022-09-30 07:40:36 +0800

    修复一个笔误

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -5,7 +5,7 @@ export default function Footer(props) {
 		<div className="lg:flex lg:justify-between max-w-screen-xl mx-auto w-full py-4">
 			<div className="text-center text-sm lg:text-left">© 2022 ManeSPACE by WeirdoGhostGang, powered by ManesLAB</div>
 			<div className="gap-4 flex justify-center text-sm items-center lg:justify-end">
-				<a href="https://www.manestudio.xyz" target="_blank" className="hover:text-gray-400">ManeStudio</a>
+				<a href="https://www.manestudio.xyz" target="_blank" className="hover:text-gray-400">ManeSTUDIO</a>
 				<a href="https://www.maneslab.xyz" target="_blank" 	className="hover:text-gray-400">ManesLAB</a>
 				<a href="https://twitter.com/manestudioxyz" target="_blank" className="hover:text-gray-400"><TwitterIcon className="icon-xs"/></a>
 				<a href="https://discord.gg/EnCUugtfVn" target="_blank" className="hover:text-gray-400"><DiscordIcon className="icon-xs"/></a>
