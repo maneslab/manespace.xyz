b80726d2310cab722a0c520c79751f7e6ff935c7
gm@maneslab.xyz <gm@maneslab.xyz>
2023-08-10 17:26:02 +0800

    added github

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,14 +1,24 @@
-import DiscordIcon from 'public/img/icons/discord.svg'
-import TwitterIcon from 'public/img/icons/twitter.svg'
+import DiscordIcon from "public/img/icons/discord.svg"
+import TwitterIcon from "public/img/icons/twitter.svg"
+import GithubIcon from "public/img/icons/github.svg"
 export default function Footer(props) {
-    return (
-		<div className="lg:flex lg:justify-between max-w-screen-xl mx-auto w-full py-4">
-			<div className="text-center text-sm lg:text-left">© 2022 ManeSPACE by WeirdoGhostGang, powered by ManesLAB</div>
-			<div className="gap-4 flex justify-center text-sm items-center lg:justify-end">
-				<a href="https://www.manestudio.xyz" target="_blank" className="hover:text-gray-400">ManeSTUDIO</a>
-				<a href="https://www.maneslab.xyz" target="_blank" 	className="hover:text-gray-400">ManesLAB</a>
-				<a href="https://twitter.com/manestudioxyz" target="_blank" className="hover:text-gray-400"><TwitterIcon className="icon-xs"/></a>
+	return (
+		<div className='lg:flex lg:justify-between max-w-screen-xl mx-auto w-full py-4'>
+			<div className='text-center text-sm lg:text-left'>© 2022 ManeSPACE by WeirdoGhostGang, powered by ManesLAB</div>
+			<div className='gap-4 flex justify-center text-sm items-center lg:justify-end'>
+				<a href='https://www.manestudio.xyz' target='_blank' className='hover:text-gray-400'>
+					ManeSTUDIO
+				</a>
+				<a href='https://www.maneslab.xyz' target='_blank' className='hover:text-gray-400'>
+					ManesLAB
+				</a>
+				<a href='https://twitter.com/manestudioxyz' target='_blank' className='hover:text-gray-400'>
+					<TwitterIcon className='icon-xs' />
+				</a>
+				<a href='https://github.com/maneslab/manestudio.xyz' target='_blank' className='hover:text-gray-400'>
+					<GithubIcon className='icon-xs' />
+				</a>
 			</div>
 		</div>
-    )
+	)
 }
